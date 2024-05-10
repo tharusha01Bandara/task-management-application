9668024a59153d6c439be5e288e03c64e0ce5e1d
tharusha01Bandara <154556642+tharusha01Bandara@users.noreply.github.com>
2024-05-11 01:26:15 +0530

    colors

--- app/src/main/java/com/example/to_doapp/data/model/ToDoData.kt
+++ app/src/main/java/com/example/to_doapp/data/model/ToDoData.kt
@@ -13,5 +13,6 @@ data class ToDoData(
     var id: Int,
     var title: String,
     var priority: Priority,
-    var description: String
+    var description: String,
+    var deadline: Long
 ): Parcelable
